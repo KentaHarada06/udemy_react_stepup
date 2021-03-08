fc0b1be0552cabd7ef750beec5340fc4a413a9b3
KentaHarada06 <72850080+KentaHarada06@users.noreply.github.com>
2021-03-09 01:06:03 +0900

    react_stepup_20210309_01

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { Router } from "./router/Router";
 import "./styles.css";
-import { InlineStyle } from "./components/InlineStyle";
-import { CssModules } from "./components/CssModules";
-import { StyledJsx } from "./components/StyledJsx";
-import { StyledComponents } from "./components/StyledComponents";
-import { Emotion } from "./components/Emotion";
 
 export default function App() {
   return (
-    <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
