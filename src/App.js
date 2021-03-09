fcbbfe07b4849261079e1bb71f89cb6f40eee223
KentaHarada06 <72850080+KentaHarada06@users.noreply.github.com>
2021-03-09 19:35:58 +0900

    react_stepup_20210308_04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecures/SearchInput";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>テスト</SecondaryButton>
-      <br />
-      <SearchInput></SearchInput>
-    </div>
-  );
+  return <Router />;
 }
